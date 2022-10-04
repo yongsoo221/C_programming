15883f57e3545ede0297f4d48102c9382fab74ee
yongsoo221 <dydtn221@naver.com>
2022-10-04 18:41:14 +0900

    add code

---
+++ hw3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    for(int i=0;i<5;i++){
+        for(int j=1;j<5-i;j++){
+            printf(" ");
+        }
+        for(int k=0;k<2*i+1;k++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
